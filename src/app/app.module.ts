79687901b228aacfc558f3c2fff493e705b58ba9
Ivan Karakolev <lewskarche7o@gmail.com>
2024-11-12 22:55:04 +0200

    Create modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './pages/home/home.module';
+import { GalleryModule } from './pages/gallery/gallery.module';
+import { CommentsModule } from './pages/comments/comments.module';
+import { ContactModule } from './pages/contact/contact.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    GalleryModule,
+    CommentsModule,
+    ContactModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
